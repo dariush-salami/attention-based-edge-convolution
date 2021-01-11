Bug fix for graph creation to allow backpropagation of gradients.

automated_graph_creation/temporal_attention_for_graph_creation.py
@@ -35,9 +35,11 @@ class TemporalSelfAttentionEdgeIndexCreatorLayer(nn.Module):
         if mask is not None:
             normalized_energy = normalized_energy.masked_fill(mask == 0, float("-1e20"))
         attention = torch.softmax(normalized_energy, dim=2)
-        attention = torch.topk(attention, self.head_edges * self.heads, 2).values.reshape(batch_size, key_len, -1)
-        edges_indices = torch.sort(attention, 2).indices
-        edges_indices = edges_indices.type(torch.float64)
+        attention = torch.topk(attention, self.head_edges * self.heads, 2)\
+            .indices\
+            .type(torch.float64)\
+            .requires_grad_(True)
+        edges_indices = torch.sort(attention, 2).values
         node_indices = self.node_indices_template \
             .repeat(batch_size).reshape(batch_size, key_len, 1) \
             .repeat(1, 1, edges_indices.shape[-1])

knn_less_train.py
@@ -107,8 +107,8 @@ def train(epoch_num):
         out, edge_indices, seq_numbers = model(data)
         graph_creation_regularizer_loss = 0
         for (index, edge_index) in enumerate(edge_indices):
-            source_seq_number = seq_numbers[edge_index[0].type(torch.long)]
-            destination_seq_number = seq_numbers[edge_index[1].type(torch.long)]
+            source_seq_number = seq_numbers[edge_index[0].type(torch.long)].requires_grad_(True)
+            destination_seq_number = seq_numbers[edge_index[1].type(torch.long)].requires_grad_(True)
             difference = torch.pow(torch.abs(destination_seq_number - source_seq_number), 3)
             difference[difference == 0] = 1
             difference = torch.mean(difference)